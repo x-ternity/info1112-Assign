65868c9e13a18df8f75e5383e61e83fb26d1c0cd
Jason <jasonjing0302@gmail.com>
2026-09-21 19:41:19 +1000

    added turning bytes into a list

--- assembler.sh
+++ assembler.sh
@@ -29,3 +29,48 @@ if [ ! -s "$1" ]; then
     echo "usage: the file is empty - no .bin file is produced"
     exit 1
 fi
+
+#Spltting .vsc file into 3 parts
+n_values=$(head -n 1 "$1")
+statics=$(tail -n +2 "$1" | head -n "$n_values")
+instructions=$(tail -n +$((n_values + 2)) "$1")
+
+#turning the bytes into a list
+bytes=()
+
+#getting the first n values
+if [ "$n_values" -gt 0 ]; then
+    while read -r value; do
+        bytes+=("$value")
+    done <<< "$statics"
+fi
+
+kind="QUIT"
+
+#getting the instruction and analyzing their opcode
+while IFS=, read -r name reg addr; do
+    if [ -z "$name" ]; then
+        continue
+    fi
+
+    if [ "$name" = "LOAD" ]; then
+        opcode=1
+    elif [ "$name" = "STORE" ]; then
+        opcode=2
+    elif [ "$name" = "ADD" ]; then
+        opcode=3
+    elif [ "$name" = "SUB" ]; then
+        opcode=4
+    elif [ "$name" = "QUIT" ]; then
+        opcode=8
+    elif [ "$name" = "PRINT" ]; then
+        opcode=9
+    fi
+
+    if [ "$name" = "ADD" ] || [ "$name" = "SUB" ]; then
+        kind="ADD/SUB"
+    fi
+
+    bytes+=( $(( (opcode << 2) + reg )) )
+    bytes+=( "$addr" )
+done <<< "$instructions"
